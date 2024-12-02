236405cf98f0eadc4bdfc579fc24543b87899e88
danielharagao <danielharagao@gmail.com>
2024-12-01 21:16:30 -0300

    LP

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("demo-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Obrigado por agendar a demonstração! Entraremos em contato em breve.");
+});
